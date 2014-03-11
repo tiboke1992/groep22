d5dffaae91111f70f3bdee06bf3b1a4349ed09e4
Tibo <tibo_hulens@hotmail.com>
2014-03-11 13:42:34 +0100

    More stuff

--- Groep22/src/be/kuleuven/assemassist/domain/Layout.java
+++ Groep22/src/be/kuleuven/assemassist/domain/Layout.java
@@ -1,5 +1,52 @@
 package be.kuleuven.assemassist.domain;
 
-public class Layout {
+import java.util.List;
 
+public class Layout {
+	
+	//1
+	private AssemblyLine assemblyLine;
+	//1..*
+	private List<WorkStation> workStations;
+	//1
+	private ConveyorBelt conveyorBelt;
+	
+	
+	public Layout(AssemblyLine assemblyLine, ConveyorBelt conveyorBelt){
+		this.setAssemblyLine(assemblyLine);
+		this.setWorkStations(WorkStation.getWorkStations());
+		this.setConveyorBelt(conveyorBelt);
+	}
+
+
+	public AssemblyLine getAssemblyLine() {
+		return assemblyLine;
+	}
+
+
+	public void setAssemblyLine(AssemblyLine assemblyLine) {
+		this.assemblyLine = assemblyLine;
+	}
+
+
+	public List<WorkStation> getWorkStations() {
+		return workStations;
+	}
+
+
+	public void setWorkStations(List<WorkStation> workStations) {
+		this.workStations = workStations;
+	}
+
+
+	public ConveyorBelt getConveyorBelt() {
+		return conveyorBelt;
+	}
+
+
+	public void setConveyorBelt(ConveyorBelt conveyorBelt) {
+		this.conveyorBelt = conveyorBelt;
+	}
+
+	
 }
