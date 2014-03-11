091231ba4ee13c3badebdbaa03d35b8a77707541
tomjo <tom.jorissen.it@gmail.com>
2014-03-11 15:34:01 +0100

    Updated DeliveryTime and added joda time library

--- Groep22/src/be/kuleuven/assemassist/domain/DeliveryTime.java
+++ Groep22/src/be/kuleuven/assemassist/domain/DeliveryTime.java
@@ -1,16 +1,32 @@
 package be.kuleuven.assemassist.domain;
 
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.joda.time.DateTime;
+
 public class DeliveryTime {
 
-	private Date startTime;
-	private Map<WorkStation, Long> timeSpentAtWorkposts;
+	private DateTime startTime;
+	private Map<Class<? extends WorkStation>, Long> timeSpentAtWorkposts;
 
 	public DeliveryTime() {
-		startTime = new Date();
+		startTime = new DateTime();
 		timeSpentAtWorkposts = new HashMap<>();
+	}
+
+	public DateTime getEstimatedDeliveryTime() {
+		int minutesToAdd;
+		Long driveTrainTime = timeSpentAtWorkposts.get(DriveTrainPost.class);
+		if (driveTrainTime != null) {
+			minutesToAdd = driveTrainTime.intValue();
+			Long accessoriesTime = timeSpentAtWorkposts.get(AccessoriesPost.class);
+			if (accessoriesTime != null)
+				minutesToAdd += accessoriesTime.intValue();
+			else
+				minutesToAdd += 60;
+		} else
+			minutesToAdd = 120;
+		return startTime.plusMinutes(minutesToAdd);
 	}
 }
