d485f8553679e0b150c3d5ba31fe9ae2743a709f
tomjo <tom.jorissen.it@gmail.com>
2014-04-09 13:24:32 +0200

    Added Spoiler

---
+++ Groep22/src/be/kuleuven/assemassist/domain/options/Spoiler.java
@@ -0,0 +1,21 @@
+package be.kuleuven.assemassist.domain.options;
+
+import static be.kuleuven.assemassist.util.Util.capitalizeFirstCharacter;
+
+/**
+ * 
+ * This enum represents the different kind of engines on a car
+ * 
+ */
+
+public enum Spoiler implements CarOption {
+
+	NONE, LOW, HIGH;
+
+	@Override
+	public String toString() {
+		if (this == NONE)
+			return "";
+		return capitalizeFirstCharacter(name());
+	}
+}
