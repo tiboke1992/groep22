17e1eac73357cc9a678f1e3a31b073676debabf4
tiboke1992 <tibo_hulens@hotmail.com>
2014-04-10 15:20:56 +0200

    With a given list of options, sorts the list

--- Groep22/src/be/kuleuven/assemassist/controller/Controller.java
+++ Groep22/src/be/kuleuven/assemassist/controller/Controller.java
@@ -4,12 +4,6 @@ import be.kuleuven.assemassist.domain.CarManufacturingCompany;
 import be.kuleuven.assemassist.event.Event;
 import be.kuleuven.assemassist.ui.UI;
 
-/**
- * 
- * This class forms a superclass for all the controllers
- * 
- */
-
 public abstract class Controller {
 
 	private CarManufacturingCompany company;
